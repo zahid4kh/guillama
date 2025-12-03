350ecbc137a53eb6d425e8789f23f279662957fc
zahid4kh <halilzahid@gmail.com>
2025-12-03 19:17:21 +0100

    set up data models for chatroom

--- src/main/kotlin/data/Models.kt
+++ src/main/kotlin/data/Models.kt
@@ -7,3 +7,17 @@ import kotlinx.serialization.Serializable
 data class AppSettings(
     val darkMode: Boolean = false
 )
+
+@Serializable
+data class Chatroom(
+    val title: String = "Default title",
+    val selectedModel: String? = null,
+    val createdAt: Long = System.currentTimeMillis(),
+    val messages: List<Chat> = emptyList()
+)
+
+@Serializable
+data class Chat(
+    val userMessage: String = "",
+    val modelMessage: String = ""
+)
