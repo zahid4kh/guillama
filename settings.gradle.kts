8b657615afb14e4deaee99a1ce6759ca973584a1
zahid4kh <halilzahid@gmail.com>
2025-12-06 12:42:57 +0100

    BSBSBS

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,8 +9,9 @@ pluginManagement {
 }
 
 plugins {
-  //https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
-  id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
+    //https://github.com/JetBrains/compose-hot-reload?tab=readme-ov-file#set-up-automatic-provisioning-of-the-jetbrains-runtime-jbr-via-gradle
+    id("org.gradle.toolchains.foojay-resolver-convention").version("0.10.0")
 }
 
 rootProject.name = "guillama"
+include("testModule")
