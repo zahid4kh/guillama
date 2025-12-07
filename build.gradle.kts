e9a2a5e3d52f712a784d4909b7a502a7b3261910
zahid4kh <halilzahid@gmail.com>
2025-12-07 12:05:54 +0100

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import java.util.UUID
 import java.util.Scanner
                 
-import org.jetbrains.compose.reload.ComposeHotRun 
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
 
 plugins {
